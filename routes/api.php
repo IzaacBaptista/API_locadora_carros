382ef43af7dda08e7dd679993701c04a6c8e363f
Izaac Baptista <izaacbaptista@gmail.com>
2022-11-21 13:10:16 -0300

    Rotas e a diferença entre Route::resource e Route::apiResource

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//route cliente
+Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
+
+//route carro
+Route::apiResource('carro', 'App\Http\Controllers\CarroController');
+
+//route locação
+Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+
+//route marca
+Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
+
+//modelo
+Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
