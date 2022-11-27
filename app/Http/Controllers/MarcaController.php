cbac389eb5cd8253a60b0469738e8c29c32af8e4
Izaac Baptista <izaacbaptista@gmail.com>
2022-11-27 20:20:13 -0300

    refactor(Marca): clean code controller

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+
+    public function __construct(Marca $marca){
+        $this->marca = $marca;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +19,7 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        $marcas = Marca::all();
+        $marcas = $this->marca->all();
         return response()->json(['marca' => $marcas]);
     }
 
@@ -26,18 +31,19 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return response()->json(['marca' => $marca, 'msg' => 'Salvo com sucesso']);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
+        $marca = $this->marca::find($id);
         return Response()->json($marca);
     }
 
@@ -45,15 +51,12 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        print_r($request->all());
-        echo '<hr>';
-        print_r($marca->getAttributes());
-
+        $marca = $this->marca->find($id);
         $marca->update($request->all());
         return response()->json(['marca' => $marca, 'msg' => 'Atualizado com sucesso']);
     }
@@ -61,12 +64,13 @@ class MarcaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+        $marca = $this->marca->find($id);
         $marca->delete();
-        return response()->json(['marca' => $marca, 'msg' => 'Excluído com sucesso']);
+        return response()->json(['marca' => $marca, 'msg' => 'Deletado com sucesso']);
     }
 }
